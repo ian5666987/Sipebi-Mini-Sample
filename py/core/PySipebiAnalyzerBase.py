f1a76408ef5d948598d010dbd5f579cbe56dab7b
Ian Kamajaya <ian.kamajaya@gmail.com>
2023-09-06 07:47:39 +0800

    Menghapus pembuatan skrip-skrip Python pada sumber code, semua skrip Python harus terdefinisi di luar sumber kode agar kode lebih rapi dan lebih mudah di-maintain

---
+++ py/core/PySipebiAnalyzerBase.py
@@ -0,0 +1,10 @@
+#kelas dasar Python bagi kelas-kelas penyuntingan lainnya
+class PySipebiAnalyzerBase:
+	diagList = [] #daftar kesalahan. List ini harus ada
+	isReady = False #bendera yang menandakan pengaturan awal sudah selesai dikerjakan. Bendera ini harus ada
+	#setup: fungsi untuk melakukan pengaturan awal sebelum 'execute' dijalankan. Fungsi ini harus ada
+	def setup(self):
+		self.isReady = True
+	#execute: fungsi untuk menjalankan penyuntingan. Fungsi ini harus ada
+	def execute(self, text):
+		pass
